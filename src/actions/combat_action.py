c3144cd89825a24b5c90bc1a5304952e4f45ebd9
cptmeeps <cptmeeps@gmail.com>
2024-11-05 02:07:47 -0800

    merge game_types into game_state

--- src/actions/combat_action.py
+++ src/actions/combat_action.py
@@ -1,7 +1,6 @@
 from typing import Dict, Tuple, Any
 import random
-from game_types import Tile
-from game_state import GameState
+from game_state import GameState, Tile
 
 def combat_action(game_state: GameState, hex_pos: Tuple[int, int]) -> GameState:
     # If no units or only one player's units, skip combat
